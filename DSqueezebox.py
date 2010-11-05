35420a22869e07647ac94182abe6c5333b5e12f8
Renophaston <renophaston@gmail.com>
2010-11-05 12:06:51 -0700

    moved Player init stuff to assign method instead

--- DSqueezebox.py
+++ DSqueezebox.py
@@ -6,28 +6,40 @@ import telnetlib
 class Player:
 	'''A class that represents a Squeezebox player.'''
 	# TODO: so far it just finds itself on the server
+	# TODO: need to deal with errors (currently it doesn't at all)
 	
-	def __init__(self, server, port, playername):
+	# TODO: also make one with the ID/MAC address
+	def assign(self, server, playername, port=9090):
+		'''Assigns this Player object to a player on the network.
+		server: the server the player is attached to
+		playername: the simple name of the player, like "KrisTouch"
+		port (default: 9090): the TELNET port the server is listening on
+		'''
 		self.server = server
-		self.port = port
 		self.playername = playername
+		self.port = port
 		# init to None, in case we don't find the named player
 		self.playerindex = None
+		self.id = None # stored as byte array when we get one
 		
 		# we have to make sure the named player exists and get its ID.
-		tn = telnetlib.Telnet(self.server, self.port)
+		tn = telnetlib.Telnet(self.server, self.port) # TODO: errors
 		
 		# get num of players connected (starts at 1)
 		tn.write(b'player count ?\n') # b makes it a byte array rather than unicode
 		response = tn.read_until(b'\n')
 		# this line will throw a ValueError if not an int
 		playercount = int (response.split(b' ')[-1].rstrip().decode())
+		
 		for i in range(playercount): # iterate through players
 			# see what the name for each player index is
+			# the text we're sending should look like: "player name # ?"
 			tn.write(b'player name ' + str(i).encode('ascii') + b' ?\n')
+			
 			response = tn.read_until(b'\n')
 			if response.split(b' ')[-1].rstrip().decode() == self.playername:
 				self.playerindex = str(i).encode('ascii');
+		
 		if self.playerindex == None: # we didn't find the name
 			print ('Player name "' + playername + '" not found on ' + server + '.')
 			tn.close()
@@ -40,4 +52,4 @@ class Player:
 		
 		# The id is the last space-separated "word" in the response.
 		# And we have to get rid of the trailing newline.
-		self.playerid = response.split(b' ')[-1].rstrip()
+		self.id = response.split(b' ')[-1].rstrip()
